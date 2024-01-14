adcd4a8b8d49e99447e58e634fa7dfb86d8350f5
ellie <elliebgdnv@gmail.com>
2024-01-15 01:24:27 +0200

    graphical_visualizer class added

--- main.cpp
+++ main.cpp
@@ -3,27 +3,28 @@
 #include <array>
 #include <queue>
 
-
+const static size_t FRAME_WIDTH = 30;
+const static size_t FRAME_HEIGHT = 30;
+#define frame_matrix std::array<std::array<std::string, FRAME_WIDTH>, FRAME_HEIGHT> 
 class frame
 {
 private:
-    const static size_t FRAME_WIDTH = 30;
-    const static size_t FRAME_HEIGHT = 30;
+
     
-    std::array<std::array<std::string, FRAME_WIDTH>, FRAME_HEIGHT> current_frame;
+   frame_matrix current_frame;
 
 public:
-    frame(const std::array<std::array<std::string, FRAME_WIDTH>, FRAME_HEIGHT> current_frame) : current_frame(current_frame)
+    frame(const frame_matrix& current_frame) : current_frame(current_frame)
     {
         
     }
 
-    std::array<std::array<std::string, FRAME_WIDTH>, FRAME_HEIGHT> get_current_frame() const
+    frame_matrix get_current_frame() const
     {
         return current_frame;
     }
 
-    void set_current_array(const std::array<std::array<std::string, FRAME_WIDTH>, FRAME_HEIGHT>& new_current_frame)
+    void set_current_array(const frame_matrix& new_current_frame)
     {
         current_frame = new_current_frame;
     }
@@ -32,11 +33,28 @@ public:
 
 class graphical_visualizer
 {
-    
+private:
+    std::queue<frame> frame_queue;
+
+public:
+    graphical_visualizer()
+    {
+        frame_queue = {};
+    }
+
+    std::queue<frame> get_frame_queue() const
+    {
+        return frame_queue;
+    }
+    void add_frame(const frame& frame)
+    {
+        frame_queue.push(frame);
+    }
+
 };
 
 int main(int, char**){
-    std::array<std::array<std::string, 30>, 30> current_frame;
+    frame_matrix current_frame;
     frame f(current_frame);
     f.get_current_frame();
 }
