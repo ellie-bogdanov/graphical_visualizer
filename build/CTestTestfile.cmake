9ffc888f5ef0b99376d29aef0093fa01e3c8e878
ellie <elliebgdnv@gmail.com>
2024-01-20 20:11:09 +0200

    added ignore files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/princess-ellie/Documents/cpp_projects/graphical_visualizer
-# Build directory: /home/princess-ellie/Documents/cpp_projects/graphical_visualizer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
